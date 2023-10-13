b4664dc50dd478b2359315d9c78201d97a775904
Satish <1202satish@gmail.com>
2023-10-13 16:47:21 +0200

    First Commit

---
+++ app/src/main/java/saty/learncompose/ComposeRecyclerView.kt
@@ -0,0 +1,146 @@
+package saty.learncompose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview
+@Composable
+fun previewListItem() {
+    //listItem(image = R.drawable.spider_man, title = "Satish", subTitle = "Software Engineer")
+    LazyColumn(content = {
+        items(getHeroList()){item->
+            listItem(image = item.image, title = item.title, subTitle = item.subTitle)
+        }
+
+    })
+}
+
+@Composable
+fun listItem(image: Int, title: String, subTitle: String) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = "Android Image",
+                modifier = Modifier
+                    .size(48.dp)
+                    .padding(8.dp)
+                    .weight(0.2f)
+            )
+            extracted(title, subTitle, Modifier.weight(0.8f))
+        }
+    }
+
+}
+
+@Composable
+fun extracted(title: String, subTitle: String, modifier: Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = title,
+            fontSize = 22.sp,
+            style = MaterialTheme.typography.displayLarge
+        )
+        Text(
+            text = subTitle,
+            fontSize = 18.sp,
+            style = MaterialTheme.typography.titleSmall,
+            fontWeight = FontWeight.Thin
+        )
+    }
+}
+
+data class superHeros(val image: Int, val title: String, val subTitle: String)
+
+fun getHeroList(): MutableList<superHeros> {
+    val heroList = mutableListOf<superHeros>()
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.super_dad, "Super Dad", "Does Things"))
+    heroList.add(superHeros(R.drawable.spider_man, "Spider Man", "Webb Power"))
+    heroList.add(superHeros(R.drawable.red_man, "Super Man", "Fly Fast"))
+    heroList.add(superHeros(R.drawable.captain_owl, "Captain Owl", "Shield"))
+    return heroList
+
+}
---
+++ app/src/main/java/saty/learncompose/MainActivity.kt
@@ -0,0 +1,132 @@
+package saty.learncompose
+
+import android.media.Image
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import saty.learncompose.ui.theme.LearnComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            previewListItem()
+        }
+    }
+}
+
+@Composable
+fun sayHello(name: String) {
+    Text(
+        text = "Hello $name",
+        color = Color.Blue,
+        fontSize = 32.sp,
+        fontStyle = FontStyle.Italic,
+        textAlign = TextAlign.End
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, name = "boxOne", widthDp = 500, heightDp = 300)
+@Composable
+fun previewer() {
+//sayHello(name = "manish")
+
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        buttonWithText()
+        textInput()
+
+        /*listItem(
+            image = R.drawable.ic_launcher_foreground,
+            name = "Satish",
+            designation = "Software Engineer"
+        )*/
+    }
+}
+
+/*@Composable
+fun listItem(image: Int, name: String, designation: String) {
+    Row {
+        image(image = image, 70.dp)
+        Column {
+            Text(text = name, fontSize = 18.sp, fontWeight = FontWeight.Bold)
+            Text(text = designation, fontSize = 18.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+
+}*/
+
+@Composable
+fun buttonWithText() {
+    Button(
+        onClick = { },
+        colors = ButtonDefaults.buttonColors(
+            contentColor = Color.Black,
+            containerColor = Color.Cyan
+        ),
+        enabled = false,
+        border = BorderStroke(5.dp, Color.Red)
+    ) {
+        image(image = R.drawable.ic_launcher_foreground)
+        sayHello(name = "Manish")
+    }
+}
+
+@Composable
+fun image(image: Int, size: Dp = 40.dp) {
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = "Android Image",
+        Modifier.size(size),
+        colorFilter = ColorFilter.tint(Color.Magenta),
+        contentScale = ContentScale.Crop,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun textInput() {
+    val state = remember { mutableStateOf("") }
+    TextField(value = state.value, onValueChange = {
+        state.value = it
+        Log.d("satish", "it")
+    })
+}
